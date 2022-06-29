52b7676df862ff7c6d79da2d6bb9591092a77d98
tajriba <quvvatullayev@gmail.com>
2022-06-29 11:55:21 +0300

    String01.py

--- String01.py
+++ String01.py
@@ -1,10 +1,14 @@
 def main(s):
     """
-    A str of several words is given. All letters are lowercase. Make sure that the first letter of each word is capitalized.
+    A str of several words is given. All letters are lowercase.
+    Make sure that the first letter of each word is capitalized.
     Args:
         s: str
     Returns:
         str: answer
     """
+    s = str(s)
     
-    return
+    return s == s.title()
+
+print(main("Mening ismim quvvatullayev ogabek"))
